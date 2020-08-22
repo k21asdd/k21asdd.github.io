4433431c196c4bdcc3277908cbddd461a6e630fa
Matthew Bian <k21asdd@gmail.com>
2020-08-22 10:04:29 +0800

    add simple vue

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,20 @@
-console.log('This would be the main JS file.');
+import vue from "vue"
+let vm = new Vue({
+    el: '#test_vue',
+    data: {
+    test_content: 'Hello Vue!',
+    list_data : ["Okla", "Okba", "Ok"],
+    }
+});
+let app = new Vue({   
+    el:"#app",   
+    data:{     
+    count: 0   
+    },   
+    methods:{     
+    add(){
+        this.count+=1;
+    }   
+    } 
+}); 
+
